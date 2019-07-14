f8a6fb328434128dfc6c2346b3070cef2e3850b9
AshutoshGupta <ashujhs@gmail.com>
2019-07-13 18:05:37 -0700

    TDD approach for all the test cases. Expect to fail.

---
+++ src/test/java/scrap/heap/refactor/builders/BalloonBuilderTest.java
@@ -0,0 +1,75 @@
+package scrap.heap.refactor.builders;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import scrap.heap.refactor.domain.Balloon;
+import scrap.heap.refactor.domain.Colors;
+import scrap.heap.refactor.domain.Material;
+
+import static org.junit.Assert.*;
+
+public class BalloonBuilderTest {
+
+  private BalloonBuilder balloonBuilder;
+
+  @Before
+  public void setUp() throws Exception {
+    balloonBuilder = new BalloonBuilder();
+  }
+
+  @Test
+  public void testBalloonColor() {
+    Balloon balloon = balloonBuilder.setBalloonColor(Colors.YELLOW).createBalloon();
+    assertNotNull("Created the balloon", balloon);
+    assertNotNull("Created the balloon with color", balloon.getBalloonColor());
+  }
+
+  @Test
+  public void testMaterial() {
+    Balloon balloon = balloonBuilder.setMaterial(Material.MYLAR).createBalloon();
+    assertNotNull("Created the balloon", balloon);
+    assertNotNull("Created the balloon with material", balloon.getMaterial());
+  }
+
+  @Test
+  public void testNumber() {
+    Balloon balloon = balloonBuilder.setNumber(4).createBalloon();
+    assertNotNull("Created the balloon", balloon);
+    assertNotNull("Created the balloon with number", balloon.getNumber());
+  }
+
+  @Test
+  public void createBalloon() {
+    Balloon balloon =
+        balloonBuilder
+            .setBalloonColor(Colors.YELLOW)
+            .setMaterial(Material.MYLAR)
+            .setNumber(4)
+            .createBalloon();
+    assertNotNull("Created the balloon", balloon);
+    assertNotNull("Created the balloon with color", balloon.getBalloonColor());
+    assertNotNull("Created the balloon with number", balloon.getNumber());
+    assertNotNull("Created the balloon with material", balloon.getMaterial());
+  }
+
+  @Test
+  public void emptyCreateBalloon() {
+    Balloon balloon = balloonBuilder.createBalloon();
+    assertNotNull("Created the balloon", balloon);
+    assertNull("Created the balloon with color", balloon.getBalloonColor());
+    assertThat(balloon.getNumber(), org.hamcrest.CoreMatchers.is(0));
+    assertNull("Created the balloon with material", balloon.getMaterial());
+  }
+
+  @Test
+  public void testPlaceOrder() {
+    balloonBuilder =
+        balloonBuilder.setBalloonColor(Colors.BROWN).setMaterial(Material.MYLAR).setNumber(5);
+    try {
+      balloonBuilder.createBalloon().placeOrder();
+    } catch (ReflectiveOperationException e) {
+      Assert.fail("Failed the balloon creation");
+    }
+  }
+}
---
+++ src/test/java/scrap/heap/refactor/builders/CakeBuilderTest.java
@@ -0,0 +1,92 @@
+package scrap.heap.refactor.builders;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import scrap.heap.refactor.domain.*;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+public class CakeBuilderTest {
+
+  private CakeBuilder cakeBuilder;
+
+  @Before
+  public void setUp() throws Exception {
+    cakeBuilder = new CakeBuilder();
+  }
+
+  @Test
+  public void testFlavor() {
+    cakeBuilder.setFlavor(Flavors.CHOCOLATE).createCake();
+    assertNotNull(cakeBuilder);
+  }
+
+  @Test
+  public void testFrostingFlavor() {
+    cakeBuilder.setFrostingFlavor(Flavors.CHOCOLATE).createCake();
+    assertNotNull(cakeBuilder);
+  }
+
+  @Test
+  public void testShape() {
+    cakeBuilder.setShape(Shapes.CIRCLE).createCake();
+    assertNotNull(cakeBuilder);
+  }
+
+  @Test
+  public void testSize() {
+    cakeBuilder.setSize(Sizes.LARGE).createCake();
+    assertNotNull(cakeBuilder);
+  }
+
+  @Test
+  public void testCakeColor() {
+    cakeBuilder.setCakeColor(Colors.BROWN).createCake();
+    assertNotNull(cakeBuilder);
+  }
+
+  @Test
+  public void testCreateCake() {
+    CakeBuilder cakeBuilder =
+        this.cakeBuilder
+            .setCakeColor(Colors.BROWN)
+            .setFlavor(Flavors.CHOCOLATE)
+            .setFrostingFlavor(Flavors.VANILLA)
+            .setShape(Shapes.CIRCLE)
+            .setSize(Sizes.SMALL);
+    Cake cake = cakeBuilder.createCake();
+    assertNotNull("Cake component missing color.", cake.getCakeColor());
+    assertNotNull("Cake component missing flavor.", cake.getFlavor());
+    assertNotNull("Cake component missing shape.", cake.getShape());
+    assertNotNull("Cake component missing size.", cake.getSize());
+    assertNotNull("Cake component missing frosting flavor.", cake.getFrostingFlavor());
+  }
+
+  @Test
+  public void test_can_create_empty_cake() {
+    Cake cake = cakeBuilder.createCake();
+    assertNull("Empty cake without color .", cake.getCakeColor());
+    assertNull("Empty cake without flavor.", cake.getFlavor());
+    assertNull("Empty cake without shape", cake.getShape());
+    assertNull("Empty cake without size", cake.getSize());
+    assertNull("Empty cake without frosting color", cake.getFrostingFlavor());
+  }
+
+  @Test
+  public void testPlaceOrder() {
+    CakeBuilder cakeBuilder =
+        this.cakeBuilder
+            .setCakeColor(Colors.BROWN)
+            .setFlavor(Flavors.CHOCOLATE)
+            .setFrostingFlavor(Flavors.VANILLA)
+            .setShape(Shapes.CIRCLE)
+            .setSize(Sizes.SMALL);
+    try {
+      cakeBuilder.createCake().placeOrder();
+    } catch (ReflectiveOperationException e) {
+      Assert.fail("Failed the cake creation");
+    }
+  }
+}
